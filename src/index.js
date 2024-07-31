fa68fba9da367988cb67f96661bb552da7e4bb4f
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-07-30 19:32:48 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+console.log("test");
+document.body.appendChild(component());
