64fab21b9f097425317a99e408d126b653ec0e98
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-08-03 17:58:45 -0700

    setup toDoitem function factory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
+function toDoItem(title, description, dueDate, priority, notes = "") {
+  return {
+    title,
+    description,
+    dueDate: new Date(dueDate),
+    priority,
+    notes,
+  };
 }
-console.log("test");
-document.body.appendChild(component());
